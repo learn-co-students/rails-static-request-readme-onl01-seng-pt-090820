096c3c00d8c7b47792c2b6f2ca3b217e1724d92d
musiccity365 <m_sackstein@yahoo.com>
2021-01-24 16:26:20 -0500

    --- Auto-Git Commit ---

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-# Create
-# new
-get '/pets/new'
-# create
-post '/pets'
 
-# Read
-
-# Update
-
-# Delete
-
-# index - pet
 get 'about', to: 'static#about'
